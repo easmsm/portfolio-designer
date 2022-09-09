3619fcbca848cc4629d3e50a4f326fe19ecd1eea
danielcnow <105885467+danielcnow@users.noreply.github.com>
2022-09-09 18:03:51 -0400

    update update function

--- controllers/api/project-routes.js
+++ controllers/api/project-routes.js
@@ -21,30 +21,55 @@ router.get("/:username", (req, res) => {
       }
     })
     .catch((err) => {
-      console.log(err);
+      // console.log(err);
       res.status(500).json(err);
     });
 });
 // FOR A GIVEN USERNAME, fetch the data from the API, Update db data.
-router.post('/update', (req,res) => {
-  Axios.get(`https://gh-pinned-repos.egoist.sh/?username=${req.body.username}`)
-        .then(async r => {
-            console.log(r)
-            projectsUpdate = r.data
-            for (let i = 0; i < projectsUpdate.length; i++) {
-              const projectdata = projectsUpdate[i]
-             await Project.update({
-                owner: projectdata.owner,
-                link: projectdata.link,
-                language: projectdata.language,
-                languageColor: projectdata.languageColor,
-                stars: projectdata.stars, 
-              }, {where: {
-                repo: projectdata.repo,
-              }}
-              )
-        }}
-)});
+router.post('/update', async (req,res) => {
+  const axiosResponse = await Axios.get(`https://gh-pinned-repos.egoist.sh/?username=danielcnow`)
+  const projectdata = axiosResponse.data
+  const recordsToUpdate = projectdata.map((data) => {
+    return {
+      owner: data.owner,
+      link: data.link,
+      language: data.language,
+      languageColor: data.languageColor,
+      stars: data.stars, }
+  })
+  const updateRes = await Project.update({
+                    owner: projectdata.owner,
+                    link: projectdata.link,
+                    language: projectdata.language,
+                    languageColor: projectdata.languageColor,
+                    stars: projectdata.stars, 
+                  }, {where: {
+                    repo: projectdata.repo,
+                  }}
+                  )
+                  console.log (updateRes)
+                  return res.send(200)
+//         .then(async r => {
+//             console.log(r)
+//             projectsUpdate = r.data
+//             for (let i = 0; i < projectsUpdate.length; i++) {
+//               const projectdata = projectsUpdate[i]
+//              await Project.update({
+//                 owner: projectdata.owner,
+//                 link: projectdata.link,
+//                 language: projectdata.language,
+//                 languageColor: projectdata.languageColor,
+//                 stars: projectdata.stars, 
+//               }, {where: {
+//                 repo: projectdata.repo,
+//               }}
+//               )
+//         }}
+// ).catch(err => {
+//   console.log(err);
+//   res.status(500).json(err);
+// })
+});
 
 router.post('/', (req, res) => {
   Project.create({
@@ -57,7 +82,7 @@ router.post('/', (req, res) => {
   })
   .then(dbProjectData => res.json(dbProjectData))
   .catch(err => {
-    console.log(err);
+    // console.log(err);
     res.status(500).json(err);
   })
 })
